f0d4d50569656befc2156d3275402fdc8a5d130f
Carlos Lopez-Camey <c.lopez@kmels.net>
2017-06-16 10:11:39 -0600

    Arregla problema cuando fecha tiene 'Ayer'

--- Descargar.py
+++ Descargar.py
@@ -1,10 +1,12 @@
 import requests
 from bs4 import BeautifulSoup
 import os
-
+import traceback
+import sys
 #######
 ### MONGO
 ######
+import pymongo
 from pymongo import MongoClient
 from pymongo.helpers import DuplicateKeyError
 client = MongoClient('localhost', 27017)
@@ -34,14 +36,16 @@ from datetime import timedelta
 from datetime import datetime
 
 def obtener_fecha(date_str):
+    assert type(date_str) is str, 'Date %s is type %s' % (date_str,type(date_str))    
     date = date_str.split(',')
     delta = timedelta(hours=0, minutes=0)
     today = datetime.now().replace(microsecond=0,second=0)
+    
     if len(date) > 1:
         day = date[0].strip()
         if day == 'Hoy':
             day = today.replace(hour=0,minute=0)
-        elif day is 'Ayer':
+        elif day == 'Ayer':
             day = today.replace(hour=0,minute=0) - timedelta(days=1)
         
         delta = datetime.strptime(date[1].strip(), '%H:%M')
@@ -56,8 +60,16 @@ def obtener_fecha(date_str):
         if month < today.month:
             year -= 1
         day = datetime(year, month, day)
-
-    return day + delta
+    
+    try:
+        return day + delta
+    except:
+        print 'Fallo al obtener fecha con %s' % date_str
+        print "Exception in user code:"
+        print '-'*60
+        exc_type, exc_value, exc_traceback = sys.exc_info()
+        traceback.print_exception(exc_type, exc_value, exc_traceback)
+        print '-'*60
 
 sopa = lambda url: BeautifulSoup(requests.get(url).text, 'html.parser') 
 #########
@@ -72,8 +84,8 @@ def listar_items(comunidad = "ciudaddeguatemala", pagina = 1):
 ### Items
 ######
 def obtener_item(item_class):
-    # Metadata de la ref
-    item_ref = item_class.find('a')
+    # Metadata de la ref    
+    item_ref = item_class.find('a')    
     paragraphs = iter(item_ref.find_all('p'))
     optionals = next(paragraphs).find_all('span')
     optclass = lambda atrs: next(c for c in atrs['class'] if 'optional' not in c and 'icon' not in c)
@@ -83,6 +95,7 @@ def obtener_item(item_class):
     price_type = next(price, None)
     price_type = price_type.text.strip() if price_type is not None else None
     date = next(paragraphs).text.strip()
+
     return {
         'title': item_ref.get('title'),
         'href': "http:" + item_ref.get('href'),
@@ -90,7 +103,8 @@ def obtener_item(item_class):
         'optionals': optionals,
         'price_tag': price_tag,
         'price_type': price_type,
-        'date': obtener_fecha(date)
+        'date': obtener_fecha(str(date)),
+        'featured': 'class' in item_class.attrs and 'featuredad' in item_class.attrs['class']
     }
     
 def descargar_item(item):
@@ -105,14 +119,16 @@ def descargar_item(item):
     desc = filter(lambda u: de_clase(u, 'item_partials_description_view'), article.find_all('p'))[0]    
     
     mapa = filter(lambda f:de_clase(f, 'map'), article.find_all('div'))[0]
-    [lati, longi] = mapa.find('a').get('href').split('/')[-1].split(',')
+    if mapa is not None:
+        [lati, longi] = mapa.find('a').get('href').split('/')[-1].split(',')
     
     item['fotos'] = filter(lambda href: href.endswith('.jpg'), map(lambda a: a.get('href'), fotos.find_all('a')))
     item['town'] = detalles.find('span').text        
     item['properties'] = dict(map(lambda li: (li.find('strong').text, li.find('span').text), opcionales.find_all('li')))
     item['desc'] = desc.text
-    item['latitude'] = lati
-    item['longitude'] = longi
+    if mapa is not None:
+        item['latitude'] = lati
+        item['longitude'] = longi
     return item
 
 def sha256(s):
@@ -120,22 +136,77 @@ def sha256(s):
     dk = hashlib.pbkdf2_hmac('sha256', s, b'OLX', 100000)
     return binascii.hexlify(dk)
     
-def guardar_item(item):
+def guardar_item(item, con_fotos = True):
     item_hash = sha256(str(item))
     item['item_hash'] = item_hash
-    print 'Guardando ... ', item_hash 
+    #print 'Guardando ... ', item_hash 
     
     try:
         doc_id = db_items.insert_one(item).inserted_id
     
         # descargar fotos ...
-        for foto_url in item['fotos']:
-            comunidad = item['href'].split('.')[0]
-            descargar_imagen(doc_id, comunidad, foto_url)
+        if con_fotos:
+            for foto_url in item['fotos']:
+                comunidad = item['href'].split('.')[0]
+                descargar_imagen(doc_id, comunidad, foto_url)
     
         return doc_id
     except DuplicateKeyError:
         return None
+
+def obtener_items(desde = datetime.now(), limite = 20, direction = pymongo.ASCENDING, ignorar_featured = True):
+    return list(db_items.find({'date': { '$lt': desde }, 'featured': False}, limit=limite).sort("date", direction))
+
+def actualizar_items():
+    ultimo_item = obtener_items(limite=1, direction = pymongo.DESCENDING)
+    
+    if len(ultimo_item) is 0:
+        ultimo_item = None
+    else:
+        ultimo_item = ultimo_item[-1]
+        print 'El ultimo item es de ', ultimo_item['date']
+
+    historia_agotada = False
+    siguiente_pagina = 1
+    
+    while ultimo_item is None or not historia_agotada:
+        prev = datetime.now()
+        print 'Descargando pagina %s ' % siguiente_pagina
+        siguientes_items = listar_items(pagina = siguiente_pagina)
+        siguiente_pagina += 1 
+        siguientes_items = map(obtener_item, siguientes_items)
+        
+        next_not_featured = next(iter([i for i in siguientes_items if not i['featured']]), None)
+
+        # si el ultimo ya esta guardado, parar
+        if ultimo_item is not None and next_not_featured['date'] < ultimo_item['date']:
+            print 'Ignorando pagina (el primero tiene fecha %s): El ultimo item tiene fecha %s' % (next_not_featured['date'], ultimo_item['date'])
+            historia_agotada = True
+            continue
+
+        for item in siguientes_items:
+            try:
+                now = datetime.now()
+                print 'Descargando ...', item['href'],
+                #item = obtener_item(sigitem)                
+                print "\"%s\"" % item['title'], '...',
+                item = descargar_item(item)
+                print len(item['fotos']), 'fotos ...',
+                doc_id = guardar_item(item, con_fotos=False)
+                print 'guardado como', doc_id,
+                print '(%s)' % item['date']
+                if not item['featured']:
+                    ultimo_item = item
+                print "(en %.2f s)" % (datetime.now() - now).total_seconds()
+            except Exception as e:
+                print 'Fallo en la pagina %s con %s ' % (siguiente_pagina-1, item['href'])
+                print "Exception in user code:"
+                print '-'*60
+                exc_type, exc_value, exc_traceback = sys.exc_info()
+                traceback.print_exception(exc_type, exc_value, exc_traceback)
+                print '-'*60
+
+        print "Pagina %s fue procesada en %.2f s" % (siguiente_pagina - 1, (datetime.now() - prev).total_seconds())
 
 #def guardar_comunidad_desde_pagina(comunidad, pagina):
     
