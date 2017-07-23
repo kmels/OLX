10abad23415ac3ed3a3a6c323404abb4a324a313
Carlos Lopez-Camey <c.lopez@kmels.net>
2017-07-23 00:13:26 -0600

    Use python 3.

--- Descargar.py
+++ Descargar.py
@@ -11,7 +11,7 @@ from pymongo import MongoClient
 from pymongo.helpers import DuplicateKeyError
 client = MongoClient('localhost', 27017)
 db = client.olx
-db_items = db.items
+db_items = db.en_alqulier
 
 ########
 ### FOTOS
@@ -64,67 +64,112 @@ def obtener_fecha(date_str):
     try:
         return day + delta
     except:
-        print 'Fallo al obtener fecha con %s' % date_str
-        print "Exception in user code:"
-        print '-'*60
+        print('Fallo al obtener fecha con %s' % date_str)
+        print("Exception in user code:")
+        print('-'*60)
         exc_type, exc_value, exc_traceback = sys.exc_info()
         traceback.print_exception(exc_type, exc_value, exc_traceback)
-        print '-'*60
+        print('-'*60)
 
 sopa = lambda url: BeautifulSoup(requests.get(url).text, 'html.parser') 
 #########
 #### LISTADO
 #########
-def listar_items(comunidad = "ciudaddeguatemala", pagina = 1):
-    url = "https://%s.olx.com.gt/piso-casa-en-venta-cat-367-p-%s" % (comunidad, pagina)    
+def listar_items(comunidad, categoria, pagina = 1):
+    url = "https://%s.olx.com.gt/%s-p-%s" % (comunidad, categoria, pagina)    
+    print("Visitando pagina %s - URL: %s" % (pagina, url))
     pagina = sopa(url)
-    return filter(lambda li: 'class' in li.attrs and 'item' in li.attrs['class'], pagina.find_all('li'))
+    return list(filter(lambda li: 'class' in li.attrs and 'item' in li.attrs['class'], pagina.find_all('li')))
+
+def zone_from_title(title):
+    import re
+    rgx = re.search('ZONA\s*(\d+)', title.upper())
+    if not rgx:
+        return None
+    return int(rgx.groups()[0])
+
+def price_gtq(price_tag):
+    if 'Q' in price_tag:
+        return price_tag.replace('Q','').replace(' ','').strip()
+    return None
+
+def price_usd(price_tag):
+    if 'USD' in price_tag:
+        return price_tag.replace('$','').replace('USD','').replace(' ','').strip()
+    return None
+    
+def optional_map(key,value):
+    if key == 'surface':
+        return (key, value.replace('m2','').strip())
+    return (key,value)
     
 #######
 ### Items
 ######
 def obtener_item(item_class):
     # Metadata de la ref    
-    item_ref = item_class.find('a')    
-    paragraphs = iter(item_ref.find_all('p'))
-    optionals = next(paragraphs).find_all('span')
-    optclass = lambda atrs: next(c for c in atrs['class'] if 'optional' not in c and 'icon' not in c)
-    optionals = dict(map(lambda o: (optclass(o.attrs), o.text), optionals))
-    price = iter(next(paragraphs).children)
-    price_tag = next(price).strip()
-    price_type = next(price, None)
-    price_type = price_type.text.strip() if price_type is not None else None
-    date = next(paragraphs).text.strip()
-
-    return {
-        'title': item_ref.get('title'),
-        'href': "http:" + item_ref.get('href'),
-        'thumbnail': item_ref.find('img').get('src'),
-        'optionals': optionals,
-        'price_tag': price_tag,
-        'price_type': price_type,
-        'date': obtener_fecha(str(date)),
-        'featured': 'class' in item_class.attrs and 'featuredad' in item_class.attrs['class']
-    }
+    try:
+        item_ref = item_class.find('a')
+        title = item_ref.get('title')
+        paragraphs = iter(item_ref.find_all('p'))
+        tiene_optionals = len(list(filter(lambda p: 'class' in p.attrs and 'optionals' in p.attrs['class'], list(item_ref.find_all('p'))))) > 0
+        if tiene_optionals:
+            optionals = next(paragraphs).find_all('span')
+            optclass = lambda atrs: next(c for c in atrs['class'] if 'optional' not in c and 'icon' not in c)
+            #optionals = dict(list(map(lambda o: optional_map(o), optionals)))
+            optionals = dict(list(map(lambda o: optional_map(optclass(o.attrs), o.text), optionals)))
+#            optclass(o.attrs), o.text
+        else:
+            optionals = dict()
+        price = iter(next(paragraphs).children)
+        price_tag = next(price).strip()
+        price_type = next(price, None)
+        price_type = price_type.text.strip() if price_type is not None else None
+        date = next(paragraphs).text.strip()
+        thumb = item_ref.find('img')
+        return {
+            'title': title,
+            'href': "http:" + item_ref.get('href'),
+            'thumbnail': thumb.get('src') if thumb is not None else None,
+            'optionals': optionals,
+            'zone': zone_from_title(title),
+            'price_usd': price_usd(price_tag),
+            'price_gtq': price_gtq(price_tag),
+            'price_type': price_type,
+            'date': obtener_fecha(str(date)),
+            'featured': 'class' in item_class.attrs and 'featuredad' in item_class.attrs['class']
+        }
+    except StopIteration:
+        print('No se pudo obtener item de: ' + item_class)
+        print('Paragraphs: ', list(item_class.find('a').find_all('p')))
+        print("Exception in user code:")
+        print('-'*60)
+        exc_type, exc_value, exc_traceback = sys.exc_info()
+        traceback.print_exception(exc_type, exc_value, exc_traceback)
+        print('-'*60)
+        return None
     
 def descargar_item(item):
+
+    print('DESCARGANDO!!!!')
+    print(item)
     page = sopa(item['href'])
     main = page.find('main')    
     fotos = main.find_all('nav')[1]        
 
     article = main.find('article')
     divs = article.find_all('div')
-    detalles = filter(lambda d: de_clase(d,'details'),divs)[0]    
-    opcionales = filter(lambda u: de_clase(u, 'item_partials_optionals_view'), article.find_all('ul'))[0]
-    desc = filter(lambda u: de_clase(u, 'item_partials_description_view'), article.find_all('p'))[0]    
+    detalles = next(filter(lambda d: de_clase(d,'details'),divs))
+    #opcionales = next(filter(lambda u: de_clase(u, 'item_partials_optionals_view'), article.find_all('ul')))
+    desc = next(filter(lambda u: de_clase(u, 'item_partials_description_view'), article.find_all('p')))
     
-    mapa = filter(lambda f:de_clase(f, 'map'), article.find_all('div'))[0]
+    mapa = next(filter(lambda f:de_clase(f, 'map'), article.find_all('div')))
     if mapa is not None:
         [lati, longi] = mapa.find('a').get('href').split('/')[-1].split(',')
     
-    item['fotos'] = filter(lambda href: href.endswith('.jpg'), map(lambda a: a.get('href'), fotos.find_all('a')))
-    item['town'] = detalles.find('span').text        
-    item['properties'] = dict(map(lambda li: (li.find('strong').text, li.find('span').text), opcionales.find_all('li')))
+    item['fotos'] = list(filter(lambda href: href.endswith('.jpg'), map(lambda a: a.get('href'), fotos.find_all('a'))))
+    item['town'] = detalles.find('span').text
+    #item['optionals'] = dict(map(lambda li: (li.find('strong').text, li.find('span').text), opcionales.find_all('li')))
     item['desc'] = desc.text
     if mapa is not None:
         item['latitude'] = lati
@@ -133,13 +178,14 @@ def descargar_item(item):
 
 def sha256(s):
     import hashlib, binascii
-    dk = hashlib.pbkdf2_hmac('sha256', s, b'OLX', 100000)
+    dk = hashlib.pbkdf2_hmac('sha256', bytes(s,'utf-8'), b'OLX', 100000)
     return binascii.hexlify(dk)
     
 def guardar_item(item, con_fotos = True):
     item_hash = sha256(str(item))
     item['item_hash'] = item_hash
-    #print 'Guardando ... ', item_hash 
+    print('Guardando ... ', item_hash )
+    print(item)
     
     try:
         doc_id = db_items.insert_one(item).inserted_id
@@ -157,59 +203,87 @@ def guardar_item(item, con_fotos = True):
 def obtener_items(desde = datetime.now(), limite = 20, direction = pymongo.ASCENDING, ignorar_featured = True):
     return list(db_items.find({'date': { '$lt': desde }, 'featured': False}, limit=limite).sort("date", direction))
 
-def actualizar_items():
-    ultimo_item = obtener_items(limite=1, direction = pymongo.DESCENDING)
+def actualizar_items(comunidad = "ciudaddeguatemala", categoria = "piso-casa-en-venta-cat-367", siguiente_pagina = 1):
+    item_mas_nuevo = obtener_items(limite=1, direction = pymongo.DESCENDING)
+    item_mas_viejo = obtener_items(limite=1, direction = pymongo.ASCENDING)
     
-    if len(ultimo_item) is 0:
-        ultimo_item = None
+    if len(item_mas_viejo) is 0:
+        item_mas_viejo = None
+        item_mas_nuevo = None
     else:
-        ultimo_item = ultimo_item[-1]
-        print 'El ultimo item es de ', ultimo_item['date']
+        item_mas_viejo = item_mas_viejo[-1]
+        item_mas_nuevo = item_mas_nuevo[-1]
+        print('El item mas viejo es del ', item_mas_viejo['date'])
+        print('El item mas nuevo es del ', item_mas_nuevo['date'])
 
     historia_agotada = False
-    siguiente_pagina = 1
     
-    while ultimo_item is None or not historia_agotada:
+    while not historia_agotada:
         prev = datetime.now()
-        print 'Descargando pagina %s ' % siguiente_pagina
-        siguientes_items = listar_items(pagina = siguiente_pagina)
+        print('Descargando pagina %s ' % siguiente_pagina)
+        siguientes_items = listar_items(comunidad, categoria, pagina = siguiente_pagina)
+        print("Numero de items: %s " % len(siguientes_items))
         siguiente_pagina += 1 
-        siguientes_items = map(obtener_item, siguientes_items)
+        siguientes_items = list(map(obtener_item, siguientes_items))
+        print("Numero de items: %s " % len(siguientes_items))
+        ### Encontrar el siguiente de forma cronologica
+        siguiente_item = next(iter([i for i in siguientes_items if i is not None and not i['featured']]), None)
+        siguiente_item = siguiente_item['date']
         
-        next_not_featured = next(iter([i for i in siguientes_items if not i['featured']]), None)
+        print("Viendo")
 
         # si el ultimo ya esta guardado, parar
-        if ultimo_item is not None and next_not_featured['date'] < ultimo_item['date']:
-            print 'Ignorando pagina (el primero tiene fecha %s): El ultimo item tiene fecha %s' % (next_not_featured['date'], ultimo_item['date'])
-            historia_agotada = True
-            continue
-
+        if item_mas_nuevo is not None:
+            if siguiente_item < item_mas_viejo['date']:
+                print('Parando aqui. El siguiente es del %s, el mas nuevo registrado fue del %s' % (siguiente_item, item_mas_nuevo['date']))
+                historia_agotada = True
+                continue 
+        #if item_mas_viejo is not None:
+            #if siguiente_item < item_mas_viejo['date']:
+            #    print 'Parando aqui. El siguiente es del %s, el mas viejo registrado fue del %s' % (siguiente_item, item_mas_viejo['date'])
+        #        historia_agotada = True
+        #        continue 
+        
         for item in siguientes_items:
             try:
                 now = datetime.now()
-                print 'Descargando ...', item['href'],
+                print('Descargando ...', item['href'])
                 #item = obtener_item(sigitem)                
-                print "\"%s\"" % item['title'], '...',
+                print("\"%s\"" % item['title'], '...', end='')
                 item = descargar_item(item)
-                print len(item['fotos']), 'fotos ...',
+                print(len(item['fotos']), 'fotos ...',end='')
                 doc_id = guardar_item(item, con_fotos=False)
-                print 'guardado como', doc_id,
-                print '(%s)' % item['date']
-                if not item['featured']:
-                    ultimo_item = item
-                print "(en %.2f s)" % (datetime.now() - now).total_seconds()
+                print('guardado como', doc_id,end='')
+                print('(%s)' % item['date'], end='')
+                print("(en %.2f s)" % (datetime.now() - now).total_seconds(),end='')
             except Exception as e:
-                print 'Fallo en la pagina %s con %s ' % (siguiente_pagina-1, item['href'])
-                print "Exception in user code:"
-                print '-'*60
+                print('Fallo en la pagina %s con %s ' % (siguiente_pagina-1, item['href']),end='')
+                print("Exception in user code:")
+                print('-'*60)
                 exc_type, exc_value, exc_traceback = sys.exc_info()
                 traceback.print_exception(exc_type, exc_value, exc_traceback)
-                print '-'*60
+                print('-'*60)
 
-        print "Pagina %s fue procesada en %.2f s" % (siguiente_pagina - 1, (datetime.now() - prev).total_seconds())
+        print("Pagina %s fue procesada en %.2f s" % (siguiente_pagina - 1, (datetime.now() - prev).total_seconds()))
+
+def to_row(item):
+    date = item['date'].strftime('%Y/%m/%d %H:%M')
+    title = item['title'].replace(',','.')
+    href = item['href']
+    lat = item['latitude']
+    lon = item['longitude']
+    bathrooms = item['optionals'].get('bathrooms','')
+    bedrooms = item['optionals'].get('bedrooms','')
+    surface = item['optionals'].get('surface','')
+    price = item['price_tag']
+    return ','.join([date, title, href, lat, lon, bathrooms, bedrooms, surface, price])
+
 
 #def guardar_comunidad_desde_pagina(comunidad, pagina):
     
 #item_refs = pagina_items(sopa(url))
 
 #guardar_item(item_refs[0])
+
+# http://dabomart.com/inmobiliaria/casas-apartamento-en-venta
+# https://vilia.com/gt/casas/venta
--- src/Lib.hs
+++ src/Lib.hs
@@ -10,27 +10,28 @@ import qualified Data.ByteString as B
 import qualified Data.ByteString.Lazy as LB
 import qualified Data.ByteString.Lazy.UTF8 as LazyUTF8
 import System.Directory
-
+import Text.HTML.TagSoup
 data OLX = OLX {
   subdomain :: String
 }
 
-data Resultado = EsViejo {
+data Item = Item {
   titulo :: String,
-  iid :: Int
-  } | EsNuevo {
+  iid :: Int,
   link :: String,
   info :: String,
   precio :: String,
   negociable :: Bool, 
   fecha :: String
-} | EsError { res :: B.ByteString }
+}
 
-esViejo (EsViejo _ _) = True
-esViejo _ = True --CHANGEME
+data Resultado = EsViejo Item | EsNuevo Item | EsError { res :: LB.ByteString }
+
+estaArchivado (EsViejo _) = True
+estaArchivado _ = True --CHANGEME
 --esViejo EsError _ _ = False
 
-esNuevo = not . esViejo
+esNuevo = not . estaArchivado
 
 comunidades :: [String]
 comunidades = ["ciudaddeguatemala", "villanueva"]
@@ -44,9 +45,20 @@ llamadas = [(com,cat) | com <- comunidades, cat <- categorias]
 desdeListado :: LB.ByteString -> IO [Resultado]
 desdeListado html = do
   --putStrLn $ "Se obtuvo " ++ (LazyUTF8.toString html)  
+  (identificar . parseTags) html >>= archivar
+
+--parsear :: 
   
-  return []
-  
+identificar :: [Tag LB.ByteString] -> IO [Item]
+identificar tags = return []
+
+--leerPagina = _
+
+archivar :: [Item] -> IO [Resultado]
+archivar is = mapM guardar is where
+  guardar :: Item -> IO Resultado
+  guardar i = return (EsViejo i)
+
 encontrarPagina :: (String, String) -> Int -> IO [Resultado]
 encontrarPagina llamada@(com, cat) p =
   let
@@ -63,6 +75,7 @@ encontrarPagina llamada@(com, cat) p =
         writeFile path (LazyUTF8.toString payload)
       
   in do
+    putStrLn url'
     r <- get url'
     let payload = r ^. responseBody
     guardarPagina p llamada payload
@@ -75,7 +88,7 @@ buscar llamada@(com,cat) = llamada `encontrarDesde` 1
     encontrarDesde :: (String, String) -> Int -> IO [Resultado]
     encontrarDesde llamada p = do      
       rs <- llamada `encontrarPagina` p
-      let (viejos, nuevos) = partition esViejo rs          
+      let (viejos, nuevos) = partition estaArchivado rs          
           hayViejos = length viejos > 0
           hayNuevos = length nuevos > 0
           
